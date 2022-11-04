c6aca7a080ea1bce8a68336ae3c48ac696807a88
Vitalii Kalinchyk <karlkory@gmail.com>
2022-11-04 21:23:48 +0200

    04 11

--- src/main/java/com/epam/rd/autotasks/ConditionStatements.java
+++ src/main/java/com/epam/rd/autotasks/ConditionStatements.java
@@ -4,8 +4,7 @@ import java.rmi.UnexpectedException;
 
 class ConditionStatements {
     public static int task1(int n) {
-        //TODO: Delete line below and write your own solution
-        throw new UnsupportedOperationException();
+        return n > 0 ? n * n : n < 0 ? -n : 0;
     }
 
 
